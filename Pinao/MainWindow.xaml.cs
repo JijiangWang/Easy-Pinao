0d397c4ce39c4a57048031d41031d86ff042dc5d
ZombieAllen <47961423+ZombieAllen@users.noreply.github.com>
2020-06-19 10:46:23 +0800

    Add files via upload

---
+++ Pinao/MainWindow.xaml.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pinao
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        SolidColorBrush backgroundColorDown = Brushes.GreenYellow;
+        SolidColorBrush backgroundColorLeft = Brushes.LightGray;
+        SolidColorBrush backgroundColorRight = Brushes.White;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            MediaPlayer mp = new MediaPlayer();
+            Uri uri = null;
+            
+
+            switch (e.Key)
+            {
+                case Key.A:
+                    key_1_Low.Background = backgroundColorDown;
+                     uri = new Uri(Environment.CurrentDirectory + "\\music\\c-1.mp3" );
+                    mp.Open(uri);
+                     mp.Play();
+                    break;
+                case Key.B:
+                    key_2_Low.Background = backgroundColorDown;
+                     uri = new Uri(Environment.CurrentDirectory + "\\music\\d-1.mp3");
+                    mp.Open(uri);
+                    mp.Play();
+                    break;
+                case Key.C:
+                    key_3_Low.Background = backgroundColorDown;
+                    uri = new Uri(Environment.CurrentDirectory + "\\music\\e-1.mp3");
+                    mp.Open(uri);
+                    mp.Play();
+                    break;
+                case Key.D:
+                    key_4_Low.Background = backgroundColorDown;
+                    uri = new Uri(Environment.CurrentDirectory + "\\music\\f-#1.mp3");
+                    mp.Open(uri);
+                    mp.Play();
+                    break;
+                case Key.E:
+                    key_5_Low.Background = backgroundColorDown;
+                    uri = new Uri(Environment.CurrentDirectory + "\\music\\g-#1.mp3");
+                    mp.Open(uri);
+                    mp.Play();
+                    break;
+                case Key.F:
+                    key_6_Low.Background = backgroundColorDown;
+                    uri = new Uri(Environment.CurrentDirectory + "\\music\\a-#1.mp3");
+                    mp.Open(uri);
+                    mp.Play();
+                    break;
+                case Key.G:
+                    key_7_Low.Background = backgroundColorDown;
+                    uri = new Uri(Environment.CurrentDirectory + "\\music\\b-1.mp3");
+                    mp.Open(uri);
+                    mp.Play();
+                    break;
+                case Key.H:
+                    key_1_High.Background = backgroundColorDown;
+                    uri = new Uri(Environment.CurrentDirectory + "\\music\\c-2.mp3");
+                    mp.Open(uri);
+                    mp.Play();
+                    break;
+                case Key.I:
+                    key_2_High.Background = backgroundColorDown;
+                    uri = new Uri(Environment.CurrentDirectory + "\\music\\d-2.mp3");
+                    mp.Open(uri);
+                    mp.Play();
+                    break;
+                case Key.J:
+                    key_3_High.Background = backgroundColorDown;
+                    uri = new Uri(Environment.CurrentDirectory + "\\music\\e-2.mp3");
+                    mp.Open(uri);
+                    mp.Play();
+                    break;
+                case Key.K:
+                    key_4_High.Background = backgroundColorDown;
+                    uri = new Uri(Environment.CurrentDirectory + "\\music\\f-#2.mp3");
+                    mp.Open(uri);
+                    mp.Play();
+                    break;
+                case Key.L:
+                    key_5_High.Background = backgroundColorDown;
+                    uri = new Uri(Environment.CurrentDirectory + "\\music\\g-#2.mp3");
+                    mp.Open(uri);
+                    mp.Play();
+                    break;
+                case Key.M:
+                    key_6_High.Background = backgroundColorDown;
+                    uri = new Uri(Environment.CurrentDirectory + "\\music\\a-#2.mp3");
+                    mp.Open(uri);
+                    mp.Play();
+                    break;
+                case Key.N:
+                    key_7_High.Background = backgroundColorDown;
+                    uri = new Uri(Environment.CurrentDirectory + "\\music\\b-2.mp3");
+                    mp.Open(uri);
+                    mp.Play();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.A:
+                    key_1_Low.Background = backgroundColorLeft;
+                    break;
+                case Key.B:
+                    key_2_Low.Background = backgroundColorLeft;
+                    break;
+                case Key.C:
+                    key_3_Low.Background = backgroundColorLeft;
+                    break;
+                case Key.D:
+                    key_4_Low.Background = backgroundColorLeft;
+                    break;
+                case Key.E:
+                    key_5_Low.Background = backgroundColorLeft;
+                    break;
+                case Key.F:
+                    key_6_Low.Background = backgroundColorLeft;
+                    break;
+                case Key.G:
+                    key_7_Low.Background = backgroundColorLeft;
+                    break;
+
+                case Key.H:
+                    key_1_High.Background = backgroundColorRight;
+                    break;
+                case Key.I:
+                    key_2_High.Background = backgroundColorRight;
+                    break;
+                case Key.J:
+                    key_3_High.Background = backgroundColorRight;
+                    break;
+                case Key.K:
+                    key_4_High.Background = backgroundColorRight;
+                    break;
+                case Key.L:
+                    key_5_High.Background = backgroundColorRight;
+                    break;
+                case Key.M:
+                    key_6_High.Background = backgroundColorRight;
+                    break;
+                case Key.N:
+                    key_7_High.Background = backgroundColorRight;
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Pinao/bin/Debug/ReadME.txt
@@ -0,0 +1,18 @@
+Welcome to use the easy pinao to play music.
+The software is easy to use if you are family with the PC keyborad.
+Hope more person who are interested in playing music in this way. No professional instrument , just PC and keyboard
+And play anytime you want.
+
+In the musical notation , you can find two lovely music. Happy birthday and 手纸 ~拝启 十五の君へ~
+
+You also can create the notation by yourselves.
+Just mapping like follow.
+1_a 2_b ...
+1 h 2 i ...
+
+Software created by Allen Wang
+If you have any suggestions , please contact : Allen.wangjijiang@hotmail.com
+
+notes: Don't press one key for long time , it will return you a huge feedback. Take care.
+
+Thanks and enjoy.
